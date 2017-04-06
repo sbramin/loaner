e2cc3a39187fa7d304c255c8926e7dabe4b40e0f
sbr <sascha.ramin@sky.uk>
2017-04-06 21:42:48 +0100

    methods not quite right...

--- go/loaner.go
+++ go/loaner.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 type investment struct {
 	Invested   int64
@@ -40,34 +43,71 @@ func grader(l loan, g grade) bool {
 	return false
 }
 
-func pickLender(lo loan, les map[string]lender) {
-	el := make([]lender, len(les))
+func pickLender(lo loan, les map[string]lender) (lender, error) {
+	el := make([]lender, 0)
 	for _, l := range les {
 		if _, ok := l.Investments[lo.Category]; !ok {
 			continue
 		} else {
-			el = append(el, l)
+			if grader(lo, l.Grade) {
+				el = append(el, l)
+			}
 		}
 	}
-	fmt.Println(el)
+	if len(el) == 0 {
+		return lender{}, fmt.Errorf("no eligble lenders for a %s loan", lo.Category)
+	} else {
+		n := rand.Int() % len(el)
+		return el[n], nil
+	}
+}
+
+func (le *lender) Invest(lo loan) error {
+	if le.Budget == 0 {
+		return fmt.Errorf("Out of cash!")
+	} else {
+		le.Budget -= 2000
+		invested := le.Investments[lo.Category].Invested
+		invested += 2000
+		fmt.Printf("%s bought £20 of %s \n", le.Name, lo.Category)
+	}
+	return nil
+}
+
+func (lo *loan) Buy() error {
+	if lo.AmountInvested == lo.Amount {
+		return fmt.Errorf("Loan already satisfied")
+	} else {
+		lo.AmountInvested += 2000
+	}
+	return nil
 }
 
 func main() {
 	lenders := make(map[string]lender)
 	loans := make([]loan, 0)
 
-	lenders["Joe"] = lender{Name: "Joe", Budget: 10000, Grade: grade{Rating: 4, Operator: ">"}, Investments: map[string]investment{"cars": investment{}, "beer": investment{MaxPercent: 0.5}}}
+	lenders["Joe"] = lender{Name: "Joe", Budget: 10000, Grade: grade{Rating: 3, Operator: ">"}, Investments: map[string]investment{"cars": investment{}, "beer": investment{MaxPercent: 0.5}}}
 	lenders["Frank"] = lender{Name: "Frank", Budget: 20000, Grade: grade{Rating: 2, Operator: ">="}, Investments: map[string]investment{"cars": investment{}, "beer": investment{MaxPercent: 0.5}}}
 	lenders["Jane"] = lender{Name: "Jane", Budget: 10000, Grade: grade{Rating: 4, Operator: ">"}, Investments: map[string]investment{"cars": investment{}, "goats": investment{}}}
-	lenders["Mary"] = lender{Name: "Mary", Budget: 10000, Grade: grade{Rating: 4, Operator: ">"}, Investments: map[string]investment{"homes": investment{}, "goats": investment{}}}
+	lenders["Mary"] = lender{Name: "Mary", Budget: 10000, Grade: grade{Rating: 3, Operator: ">"}, Investments: map[string]investment{"homes": investment{}, "goats": investment{}}}
 
 	loans = append(loans, loan{Amount: 100000, Category: "beer", Rating: 4})
-	loans = append(loans, loan{Amount: 100000, Category: "burgers", Rating: 4})
+	loans = append(loans, loan{Amount: 100000, Category: "burgers", Rating: 3})
 	loans = append(loans, loan{Amount: 100000, Category: "cars", Rating: 2})
 
 	for _, l := range loans {
-		pickLender(l, lenders)
-	}
+		for l.Amount > l.AmountInvested {
+			lender, err := pickLender(l, lenders)
+			if err != nil {
+				fmt.Println(err)
+				//break
+			} else {
+				lender.Invest(l)
+				l.Buy()
+			}
+		}
 
-	//fmt.Println(loans)
+		fmt.Println(l.AmountInvested)
+	}
 }
